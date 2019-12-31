6a4f7067914054032175332194c012e5f63ea00a
rayna williams <Raynawilliams123@gmail.com>
2019-12-30 21:55:34 -0700

    origin.master

---
+++ script.js
@@ -0,0 +1,30 @@
+// variables:
+
+
+
+
+
+
+//AJAX calls
+
+
+
+
+
+
+//functions and for loops to cycle through the APIs
+
+
+
+
+
+
+//buttons that are activated on "click"
+
+
+
+
+//render buttons
+
+
+
