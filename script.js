0a43e2ca5ca4c26d025ee3cd0ae28e8acfeb096b
rayna williams <Raynawilliams123@gmail.com>
2020-01-01 18:27:52 -0700

    origin.master

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 // variables:
-
+var city =$(this).attr("data-city");
+var date =$(this).attr("data-city-date");
+var image =$(this).attr("data-city-icon+image");
+var temp =$(this).attr("data-city-temp");
+var humidity =$(this).attr("data-city-humidity");
+var wind =$(this).attr("data-city-wind+speed");
+var uvIndex=$(this).attr("data-city-uv+index");
+var queryUrl = "http://api.openweathermap.org/data/2.5/forecast?id=524901&1ec3937220c2fe8f3b996ee4ee157942=?search";
 
 
 
