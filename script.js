12fd26bfd4d956ad6631208115d92a324681ad3d
rayna williams <Raynawilliams123@gmail.com>
2020-01-02 21:40:24 -0700

    origin.master

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 // variables:
-var city = $(this).attr("data-city");
-var date = $(this).attr("data-city-date");
-var image = $(this).attr("data-city-icon+image");
-var temp = $(this).attr("data-city-temp");
-var humidity = $(this).attr("data-city-humidity");
-var wind = $(this).attr("data-city-wind+speed");
-var uvIndex = $(this).attr("data-city-uv+index");
-var queryUrl = "http://api.openweathermap.org/data/2.5/forecast?id=1ec3937220c2fe8f3b996ee4ee157942=?search";
+var city = results[i].city;
+var date = results[i].date;
+var image = results[i].image;
+var temp = results[i].temperature;
+var humidity = results[i].humidity;
+var wind = results[i].wind - speed;
+var uvIndex = results[i].uv - index;
+var queryUrl = "https://api.openweathermap.org/data/2.5/weather?q={response}";
 var results = "response.data";
 
 
@@ -20,8 +20,30 @@ $("city-btn").on("click", function () {
     }).then(function (response) {
         console.log(response);
         for (var i = 0; i < results.length; i++) {
-            var cityDiv = $("<div>")
-            var p = $("<p>");
+            var cityDiv = $("<div>");
+            var cityP = $("<p>").text("City: " + city);
+            var cityImage = $("<img>");
+            image.attr("src", results[i].images.fixed_height.url);
+            var dateP = $("<p>").text("Date:" + date);
+            var tempP = $("<p>").text("Temperature" + temp);
+            var humidP = $("<p>").text("Humidity" + humidity);
+            var windP = $("<p>").text("Wind-Speed" + wind);
+            var uvP = $("<p>").text("Uv-Index" + uvIndex);
+            cityDiv.prepend(cityP);
+            cityDiv.prepend(cityImage);
+            cityDiv.prepend(dateP);
+            cityDiv.prepend(tempP);
+            cityDiv.prepend(humidP);
+            cityDiv.prepend(windP);
+            cityDiv.prepend(uvP);
+            $("#City-appears-here").prepend(cityDiv);
+            $("#Date-appears-here").prepend(cityDiv);
+            $("#img-appears-here").prepend(cityDiv);
+            $("#Temperature-appears-here").prepend(cityDiv);
+            $("#Humidity-appears-here").prepend(cityDiv);
+            $("#Wind-Speed-appears-here").prepend(cityDiv);
+            $("#Uv-Index-appears-here").prepend(cityDiv);
+
         }
 
     });
