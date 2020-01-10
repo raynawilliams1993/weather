24de6f284340f4e05201a712056783cc5dfc27f0
rayna williams <Raynawilliams123@gmail.com>
2020-01-09 20:19:10 -0700

    origin.master

--- script.js
+++ script.js
@@ -30,32 +30,47 @@ $("#city-btn").on("click", function () {
         method: "GET"
     }).then(function (response) {
         console.log(response);
-        for (var i = 0; i < results.length; i++) {
-            var cityDiv = $("<div>");
-            var cityP = $("<p>").text("City: " + city);
-            var cityImage = $("<img>");
-            image.attr("src", results[i].images.fixed_height.url);
-            var dateP = $("<p>").text("Date:" + date);
-            var tempP = $("<p>").text("Temperature" + temp);
-            var humidP = $("<p>").text("Humidity" + humidity);
-            var windP = $("<p>").text("Wind-Speed" + wind);
-            var uvP = $("<p>").text("Uv-Index" + uvIndex);
-            cityDiv.prepend(cityP);
-            cityDiv.prepend(cityImage);
-            cityDiv.prepend(dateP);
-            cityDiv.prepend(tempP);
-            cityDiv.prepend(humidP);
-            cityDiv.prepend(windP);
-            cityDiv.prepend(uvP);
-            $("#City-appears-here").prepend(cityDiv);
-            $("#Date-appears-here").prepend(cityDiv);
-            $("#img-appears-here").prepend(cityDiv);
-            $("#Temperature-appears-here").prepend(cityDiv);
-            $("#Humidity-appears-here").prepend(cityDiv);
-            $("#Wind-Speed-appears-here").prepend(cityDiv);
-            $("#Uv-Index-appears-here").prepend(cityDiv);
+        var temp = response.main.temp;
+        var humidity = response.main.humidity;
+        var wind = response.wind.speed;
+        console.log(wind);
 
-        }
+
+        $("#cities-view").html(`
+        <ul>
+       
+        <li class="list-group-item list-group-item-primary">Temperature: ${temp}</li>
+        <li class="list-group-item list-group-item-dark"> Humidity: ${humidity}</li>
+        <li class="list-group-item list-group-item-primary"> Wind-Speed: ${wind}</li>
+       
+         </ul>`
+        );
+        // for (var i = 0; i < results.length; i++) {
+        //     var cityDiv = $("<div>");
+        //     var cityP = $("<p>").text("City: " + city);
+        //     var cityImage = $("<img>");
+        //     image.attr("src", results[i].images.fixed_height.url);
+        //     var dateP = $("<p>").text("Date:" + date);
+        //     var tempP = $("<p>").text("Temperature" + temp);
+        //     var humidP = $("<p>").text("Humidity" + humidity);
+        //     var windP = $("<p>").text("Wind-Speed" + wind);
+        //     var uvP = $("<p>").text("Uv-Index" + uvIndex);
+        //     cityDiv.prepend(cityP);
+        //     cityDiv.prepend(cityImage);
+        //     cityDiv.prepend(dateP);
+        //     cityDiv.prepend(tempP);
+        //     cityDiv.prepend(humidP);
+        //     cityDiv.prepend(windP);
+        //     cityDiv.prepend(uvP);
+        //     $("#City-appears-here").prepend(cityDiv);
+        //     $("#Date-appears-here").prepend(cityDiv);
+        //     $("#img-appears-here").prepend(cityDiv);
+        //     $("#Temperature-appears-here").prepend(cityDiv);
+        //     $("#Humidity-appears-here").prepend(cityDiv);
+        //     $("#Wind-Speed-appears-here").prepend(cityDiv);
+        //     $("#Uv-Index-appears-here").prepend(cityDiv);
+
+        // }
 
     });
 });
